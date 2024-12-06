cfdcb87f1885f4f68f0f11ffb8e858b453b19569
deividas strole <deiwis@yahoo.com>
2024-12-05 17:59:14 -0800

    trying ai

--- lawander/src/main/java/com/example/lawander/controller/LawanderController.java
+++ lawander/src/main/java/com/example/lawander/controller/LawanderController.java
@@ -1,13 +1,33 @@
 package com.example.lawander.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LawanderController {
+
+    private final ChatClient chatClient;
+
+    public LawanderController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
 
     @GetMapping("/welcome")
     public String showTest() {
         return "test";
     }
+
+    @GetMapping("/ai")
+    @ResponseBody
+    String generation() {
+        return this.chatClient.prompt()
+                .user("tell me a joke")
+                .call()
+                .content();
+    }
+
+
 }
+
